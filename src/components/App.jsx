f8bc27b09b9f08d734a3b1a253873201741c1ad6
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2025-10-28 22:39:20 +0200

    part 2 complite

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import Profile from "./Profile/Profile";
 import user from "./data/user.json";
+import Statistics from "./Statistics/Statistics";
+import data from "./data/data.json";
 
 export const App = () => {
   return (
@@ -10,7 +12,11 @@ export const App = () => {
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-      />      
-   </div>  
+      />  
+      <Statistics title="Upload stats"
+        stats={data} />
+      <Statistics stats={data}/>
+    </div>  
+
   );
 };
