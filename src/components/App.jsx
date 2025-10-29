9f2a0ee48ef02ed4bc37c0762ed6073c97f52753
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2025-10-29 19:25:44 +0200

    part 4 inprogress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,8 @@ import Statistics from "./Statistics/Statistics";
 import data from "./data/data.json";
 import friends from './data/friends.json';
 import FriendList from "./FriendList/FriendList";
-
+import transactions from "./data/transactions.json";
+import TransactionHistory from "./TransactionHistory/TransactionHistory"
 
 export const App = () => {
   return (
@@ -20,6 +21,7 @@ export const App = () => {
         stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </div>  
 
   );
